f7eea3d5ed7ff4a38b44ee369cffba2f35dad3f9
Anwar Hossain <cseanwar@gmail.com>
2026-03-31 18:27:26 +0200

    Added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,86 @@
+const Navbar = () => {
+  return (
+    <div className="shadow-sm py-2 bg-[#FFFFFF]">
+      <div className="navbar container mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-[#101727]"
+            >
+              <li>
+                <a>Products</a>
+              </li>
+              <li>
+                <a>Features</a>
+              </li>
+              <li>
+                <a>Pricing</a>
+              </li>
+              <li>
+                <a>Testimonials</a>
+              </li>
+              <li>
+                <a>FAQ</a>
+              </li>
+            </ul>
+          </div>
+          <a className="text-[40px] font-extrabold bg-linear-to-r from-[#4f39f6] to-[#9514fa] bg-clip-text text-transparent">
+            DigiTools
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-[#101727] font-semibold text-base gap-6">
+            <li>
+              <a>Products</a>
+            </li>
+            <li>
+              <a>Features</a>
+            </li>
+            <li>
+              <a>Pricing</a>
+            </li>
+            <li>
+              <a>Testimonials</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end flex gap-6">
+          <div className="relative">
+            <img className="w-6 h-6" src="/shopping-cart.png" alt="" />
+            
+              <div className="badge badge-error absolute -top-4 -right-4 w-7 h-7 rounded-full text-white font-bold">
+                
+              </div>
+          </div>
+          <p className="text-[#101727] font-semibold text-base">Login</p>
+          <a className="btn rounded-full bg-linear-to-r from-[#4f39f6] to-[#9514fa] text-base font-semibold text-[#FFFFFF]">
+            Get Started
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
